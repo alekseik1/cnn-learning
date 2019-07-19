43740eba944247142ed7e9cab3ca162cc76454e4
alekseik1 <1alekseik1@gmail.com>
2019-07-19 16:46:28 +0200

    feat: bag separator

--- pipeline.py
+++ pipeline.py
@@ -1,13 +1,25 @@
 from keras.datasets import mnist
-from preprocessing import ImageScaler, preprocess_categories
+from preprocessing import ImageScaler, preprocess_categories, create_bags
 from sklearn.pipeline import Pipeline
 from model import MainModel
+import numpy as np
 
 MODEL_NAME = 'model_trained.h5'
 
 if __name__ == '__main__':
     # Load dataset
     (x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+    ##############################
+    # Bag example
+    x_train_zeros = x_train[np.argwhere(y_train == 0).reshape(-1)]
+    x_test_zeros = x_test[np.argwhere(y_test == 0).reshape(-1)]
+    x_train_nonzeros = x_train[np.argwhere(y_train != 0).reshape(-1)]
+    x_test_nonzeros = x_test[np.argwhere(y_test != 0).reshape(-1)]
+    ##############################
+
+    x_bag, y_bag = create_bags(x_train_zeros, x_train_nonzeros)
+
     y_train, y_test = preprocess_categories(y_train), preprocess_categories(y_test)
 
     pipeline = Pipeline([
--- preprocessing.py
+++ preprocessing.py
@@ -3,6 +3,31 @@ from sklearn.preprocessing import FunctionTransformer
 import numpy as np
 
 
+def create_bags(zero_x, nonzero_x, bag_size=100, percentage=0.01):
+    zeros_in_bag = np.ceil(percentage*bag_size).astype(int)
+    total_bags = np.ceil(
+        (len(zero_x) + len(nonzero_x))/bag_size
+    ).astype(int)
+    result_x = np.empty(
+        [total_bags, bag_size, *zero_x.shape[1:]]
+    )
+    result_y = np.empty([total_bags])
+    for i in range(total_bags):
+        # Numpy random.choice needs 1-D array
+        zero_choice = zero_x[
+            np.random.choice(len(zero_x), zeros_in_bag)
+        ]
+        # Fill rest data with nonzero_x
+        nonzero_choice = nonzero_x[
+            np.random.choice(len(nonzero_x), bag_size - zeros_in_bag)
+        ]
+        result_x[i] = np.concatenate((zero_choice, nonzero_choice))
+        # If percentage is 0, we don't have zeros in a bag
+        # so that the label is 1
+        result_y[i] = int(percentage == 0)
+    return result_x, result_y
+
+
 def preprocess_categories(y_data):
     num_classes = len(np.unique(y_data))
     y_data = to_categorical(y_data, num_classes)
