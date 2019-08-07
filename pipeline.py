1da3383ff9b147a6d639ca48529482af8fe3b8b6
alekseik1 <1alekseik1@gmail.com>
2019-08-07 11:42:41 +0200

    feat: extend dataset with same images

--- pipeline.py
+++ pipeline.py
@@ -1,5 +1,6 @@
 from keras.datasets import mnist
 from preprocessing import ImageScaler, split_into_bags, add_color_channel
+from utils import extend_bags_permutations
 from sklearn.pipeline import Pipeline
 from model import BagModel
 
@@ -21,7 +22,8 @@ if __name__ == '__main__':
     bags_x, bags_y = split_into_bags(x_train, y_train,
                                      bag_size=100,
                                      zero_bags_percent=0.5,
-                                     zeros_in_bag_percentage=0.15)
+                                     zeros_in_bag_percentage=0.05)
+    bags_x, bags_y = extend_bags_permutations(bags_x, bags_y, total_num=10)
     test_bags_x, test_bags_y = split_into_bags(x_test, y_test,
                                                bag_size=100,
                                                zero_bags_percent=0.5,
--- utils.py
+++ utils.py
@@ -1,3 +1,6 @@
+from itertools import islice, permutations
+
+import numpy as np
 from keras.models import Input
 from keras import Model
 
@@ -7,3 +10,19 @@ def test_layer(layer, data):
     model = Model(inputs=[input_layer], outputs=layer(input_layer))
     model.compile(optimizer='adadelta', loss='binary_crossentropy')
     return model.predict(data)
+
+
+def extend_bags_permutations(x_bags, labels, total_num=100):
+    """
+    Shuffles elements in each bag, and then returns the dataset extended by permutations
+    :param x_bags: original bags to permutate
+    :param labels: labels corresponding to bags
+    :param total_num: total numbers of permutations for each bag
+    :return: (x_bags_new, y_labels_new) -- extended array of bags and corresponding labels
+    """
+    result_x, result_y = [], []
+    for i in range(len(x_bags)):
+        perms = list(islice(permutations(x_bags[i]), total_num))
+        result_x.extend(perms)
+        result_y.extend([labels[i] for _ in range(len(perms))])
+    return np.array(result_x), np.array(result_y)
