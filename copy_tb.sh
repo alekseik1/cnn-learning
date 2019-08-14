312d5a29feadcae9951d9b89097a6d9577c9fada
alekseik1 <1alekseik1@gmail.com>
2019-08-14 17:04:50 +0200

    feat: cluster download scripts

---
+++ copy_tb.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+scp -r login.leonhard.ethz.ch:github_projects/cnn-learning/tlogs/ /Users/akoyharin/PycharmProjects/cnn-learning/
---
+++ copy_weigths.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+scp -r login.leonhard.ethz.ch:github_projects/cnn-learning/trained/ /Users/akoyharin/PycharmProjects/cnn-learning/
