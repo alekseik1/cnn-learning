e8093b7bdf2b470483019e9d71e8518b50f105e6
alekseik1 <1alekseik1@gmail.com>
2019-08-19 16:34:22 +0200

    conf: introduce 'test' config

--- config.py
+++ config.py
@@ -16,8 +16,17 @@ class Config:
     healthy_dir = 'healthy_imgs'
 
 
+# For small learning checks
 class DebugConfig(Config):
     debug = True
+    verbose = True
+    epochs = 10
+    diseased_dir = 'debug_imgs/diseased'
+    healthy_dir = 'debug_imgs/healthy'
+
+
+# For small evaluation and model loading checks
+class TestConfig(Config):
     verbose = True
     epochs = 10
     weights_file = 'model_trained.h5'
@@ -25,6 +34,7 @@ class DebugConfig(Config):
     healthy_dir = 'debug_imgs/healthy'
 
 
+# For training on real data
 class ProductionConfig(Config):
     epochs = 50
     diseased_dir = '/nfs/nas22.ethz.ch/fs2202/biol_imsb_claassen_1/corino/Scratch/EmanuelDatasets/tryp_0.01/diseased/1'
@@ -36,3 +46,5 @@ def load_config(args):
         return DebugConfig
     elif args.config_type == 'production':
         return ProductionConfig
+    elif args.config_type == 'test':
+        return TestConfig
