ec23d71e5a86751afbfdec0dffc34dcdaceeeaba
alekseik1 <1alekseik1@gmail.com>
2019-08-19 14:10:35 +0200

    ref: move all script options to config

--- config.py
+++ config.py
@@ -2,30 +2,40 @@ class Config:
     """
     Basic config
     """
-    work_dir = 'trained'
+    weights_dir = 'trained'
     save_best_only = True
     epochs = 10
     verbose = False
     debug = False
-    load_part = 1.0
-    load_from = None
+    weights_name = None
     tensorboard_dir = 'tlogs'
     bag_size = 50
     batch_size = 10
-    diseased_dir = 'diseased_imgs'
-    healthy_dir = 'healthy_imgs'
 
 
 class DebugConfig(Config):
     debug = True
     verbose = True
     epochs = 10
-    load_from = 'model_trained.h5'
-    diseased_dir = 'debug_imgs/diseased'
-    healthy_dir = 'debug_imgs/healthy'
+
+
+class TestConfig(Config):
+    debug = True
+    verbose = True
+    epochs = 10
+    weights_name = 'model_trained.h5'
 
 
 class ProductionConfig(Config):
     epochs = 50
     diseased_dir = '/nfs/nas22.ethz.ch/fs2202/biol_imsb_claassen_1/corino/Scratch/EmanuelDatasets/tryp_0.01/diseased/1'
     healthy_dir = '/nfs/nas22.ethz.ch/fs2202/biol_imsb_claassen_1/corino/Scratch/EmanuelDatasets/tryp_0.01/control/1'
+
+
+def load_config(config_type):
+    if config_type == 'debug':
+        return DebugConfig
+    elif config_type == 'production':
+        return ProductionConfig
+    elif config_type == 'test':
+        return TestConfig
--- pipeline.py
+++ pipeline.py
@@ -1,22 +1,24 @@
 from utils import parse_args
 from image_loader import KerasImageLoader
+from config import load_config
 import os
 
 
 if __name__ == '__main__':
     args = parse_args()
+    config = load_config(args.config_type)
 
     from model import BagModel
     loader = KerasImageLoader(classes_directory='debug_imgs', bag_size=10, batch_size=2)
 
-    model_instance = BagModel(num_epochs=args.epochs,
+    model_instance = BagModel(num_epochs=config.epochs,
 
-             # TODO: rename to `load_weights`
-             load_path=(os.path.join(os.getcwd(), args.work_dir, args.load_from)
-                        if args.load_from else None),
-             verbose=args.verbose,
-             batch_size=args.batch_size,
-             save_best_only=args.save_best_only, tensorboard_dir=args.tensorboard_dir,
-             debug=args.debug)
+                              # TODO: rename to `load_weights`
+                              load_path=(os.path.join(os.getcwd(), config.weights_dir, config.weights_name)
+                                         if config.weights_name else None),
+                              verbose=config.verbose,
+                              batch_size=config.batch_size,
+                              save_best_only=config.save_best_only, tensorboard_dir=config.tensorboard_dir,
+                              debug=config.debug)
     model = model_instance._create_model((loader.bag_size, *loader.image_shape))
-    model.fit_generator(loader, steps_per_epoch=loader.total_images / args.batch_size, epochs=args.epochs)
+    model.fit_generator(loader, steps_per_epoch=loader.total_images / config.batch_size, epochs=config.epochs)
