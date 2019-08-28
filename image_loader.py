caa220b8a2d1e92bca0d57ba8c19f8fff451d15b
alekseik1 <1alekseik1@gmail.com>
2019-08-28 14:15:21 +0200

    feat: tools for mnist loading

--- image_loader.py
+++ image_loader.py
@@ -1,7 +1,78 @@
 import numpy as np
 from sklearn.model_selection import train_test_split
 from preprocessing import extend_to_bagsize
-from utils import split_into_bags, logger
+from utils import split_into_bags, logger, add_color_channel
+
+
+def _separate_by_labels(x_data, y_data):
+    return x_data[np.argwhere(y_data == 0).reshape(-1)], x_data[np.argwhere(y_data != 0).reshape(-1)]
+
+
+def mnist_split_into_bags(x_data, y_data, zero_bags_percent=0.5, bag_size=100, zeros_in_bag_percentage=0.15):
+    """
+    Splits data in bags so that it has `zero_bags_percent` bags with `bag_percentage` zero rate out of total bags
+    and (1 - zero_bags_percent) of bags without any zero element
+
+    :param x_data: source data
+    :param y_data: source labels
+    :param zero_bags_percent: percentage of bags containing at least one zero element
+    :param bag_size: size of one bag
+    :param zeros_in_bag_percentage: percentage of zero elements in one zero bag
+    :return: tuple (x_bags_split, y_bags_split) where `x_bags` split is
+    (bags_total, bag_size, img_width, img_height) array
+    and `y_bags_split` is a 1-D vector (bags_total,) of numbers: 0 or 1
+    """
+    zero_x, nonzero_x = _separate_by_labels(x_data, y_data)
+    total_bags_number = np.ceil(len(x_data)/bag_size).astype(int)
+    zero_bags_number = np.ceil(total_bags_number*zero_bags_percent).astype(int)
+
+    x_bags_split = np.empty([total_bags_number, bag_size, *x_data.shape[1:]])
+    y_bags_split = np.empty([total_bags_number], dtype=np.int)
+
+    # Create zero bags
+    for i in range(zero_bags_number):
+        x_bags_split[i], y_bags_split[i] = create_bag(zero_x, nonzero_x, bag_size, percentage=zeros_in_bag_percentage)
+    # Create nonzero bags
+    for i in range(zero_bags_number, total_bags_number):
+        x_bags_split[i], y_bags_split[i] = create_bag(zero_x, nonzero_x, bag_size, percentage=0)
+
+    return x_bags_split, y_bags_split
+
+
+def create_bag(zero_x, nonzero_x, bag_size=100, percentage=0.01):
+    """
+    For given `zero_x`, `nonzero_x` features, constructs one bag containing `bag_size*percentage` zeros.
+
+    :param zero_x: source input (zero-labeled)
+    :param nonzero_x: source input (nonzero-labeled)
+    :param bag_size: size of a bag
+    :param percentage: percentage of zeros
+    :return: tuple (bag, label). `bag` is (bag_size, img_width, img_height) array, `label` is 1-D number -- one of 1 or 0
+    """
+    zeros_in_bag = np.ceil(percentage*bag_size).astype(int)
+    # Numpy random.choice needs 1-D array
+    zero_choice = zero_x[np.random.choice(len(zero_x), zeros_in_bag)]
+    # Fill rest data with nonzero_x
+    nonzero_choice = nonzero_x[np.random.choice(len(nonzero_x), bag_size - zeros_in_bag)]
+    result_x = np.concatenate((zero_choice, nonzero_choice))
+    result_y = int(percentage == 0)
+    return result_x, result_y
+
+
+def load_mnist(bag_size, zeros_in_bag, zero_bags):
+    from keras.datasets import mnist
+    # TODO: MNIST loader
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+    x_train, x_test = add_color_channel(x_train), add_color_channel(x_test)
+    bags_x, bags_y = mnist_split_into_bags(x_train, y_train,
+                                           bag_size=bag_size,
+                                           zero_bags_percent=zero_bags,
+                                           zeros_in_bag_percentage=zeros_in_bag)
+    test_bags_x, test_bags_y = mnist_split_into_bags(x_test, y_test,
+                                                     bag_size=bag_size,
+                                                     zero_bags_percent=zero_bags,
+                                                     zeros_in_bag_percentage=zeros_in_bag)
+    return (bags_x, bags_y), (test_bags_x, test_bags_y)
 
 
 def load_and_split_data(diseased_dir, load_part, healthy_dir, bag_size):
